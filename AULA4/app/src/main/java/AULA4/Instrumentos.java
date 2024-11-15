f2f4567f7ca96c81fee1b2ae20fe2b6e45ac88fa
Thesantosz <162642046+Thesantosz@users.noreply.github.com>
2024-11-14 21:48:32 -0300

    Add files via upload

---
+++ AULA4/app/src/main/java/AULA4/Instrumentos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package AULA4;
+
+/**
+ *
+ * @author LEANDROHENRIQUESANTO
+ */
+public interface Instrumentos {
+    void tocar();
+    void afinar();
+}
