28c47d3b9f7c8906511a9591dfecb355755e6466
gguariniri <162642046+Thesantosz@users.noreply.github.com>
2025-02-19 19:19:26 -0300

    Add files via upload

---
+++ trabalhoArrray/Arrays.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalhoArrray;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LEANDROHENRIQUESANTO
+ */
+public class Arrays {
+    
+    private static ArrayList<Pokemon> mochila = new ArrayList();
+    private static ArrayList<Pokemon> estoque = new ArrayList();
+    
+    
+    
+}
