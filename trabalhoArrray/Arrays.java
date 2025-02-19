545fd31232dd6b704db9f46fdc1776565c579426
gguariniri <162642046+Thesantosz@users.noreply.github.com>
2025-02-19 19:26:50 -0300

    Update Arrays.java

--- trabalhoArrray/Arrays.java
+++ trabalhoArrray/Arrays.java
@@ -14,6 +14,20 @@ public class Arrays {
     
     private static ArrayList<Pokemon> mochila = new ArrayList();
     private static ArrayList<Pokemon> estoque = new ArrayList();
+
+estoque.add(new Pokemon("Boulbassar", "Grass and Poison", 9));
+estoque.add(new Pokemon("Charmander", "Fire", 9));
+estoque.add(new Pokemon("Squirtle", "Water", 9));
+estoque.add(new Pokemon("Chikorita", "Grass", 10));
+estoque.add(new Pokemon("Cyndaquil", "Water", 10));
+estoque.add(new Pokemon("Totodile", "Water", 10));
+estoque.add(new Pokemon("Treecko", "Grass", 11));
+estoque.add(new Pokemon("Torchic", "Fire", 11));
+estoque.add(new Pokemon("Mudkip", "Water", 11));
+estoque.add(new Pokemon("Turtwig", "Grass", 12));
+estoque.add(new Pokemon("Piplup", "Fire", 12));
+estoque.add(new Pokemon("Chimchar", "Water", 12));
+
     
     
     
