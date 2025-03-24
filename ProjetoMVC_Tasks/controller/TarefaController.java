488036db35e860b99fdcdc9274df4ffe0a9d5284
gguariniri <162642046+Thesantosz@users.noreply.github.com>
2025-03-24 19:37:43 -0300

    Update TarefaController.java

--- ProjetoMVC_Tasks/controller/TarefaController.java
+++ ProjetoMVC_Tasks/controller/TarefaController.java
@@ -4,10 +4,49 @@
  */
 package ProjetoMVC_Tasks.controller;
 
+import ProjetoMVC_Tasks.model.Tarefa;
+import ProjetoMVC_Tasks.model.TarefaDAO;
+
 /**
  *
  * @author LEANDROHENRIQUESANTO
  */
 public class TarefaController {
     
+    
+    public static String adicionarTarefa(String titulo, String descricao, String dataString, String status){
+        
+        try{
+        
+        Tarefa tarefa = new Tarefa(titulo, descricao, dataString);
+        TarefaDAO tarefaDAO = new TarefaDAO();
+        tarefaDAO.adicionarTarefa(tarefa);
+        
+        return 
+                "Cadastro da tarefa realizado!";
+                }catch (NumberFormatException e){
+return "Erro:" + e.getMessage();
 }
+    }
+    
+    public static String atualizarTarefa(String novoTitulo, String novaDescricao, String novaData, int id){
+        
+        try{
+          TarefaDAO novaTarefa = new TarefaDAO();
+          novaTarefa.atualizarTarefa(novoTitulo, novaDescricao, novaData, id);
+            
+        }catch (Exception e) {
+                // Caso ocorra algum erro durante a execução do PreparedStatement, 
+                // ele é capturado aqui.
+                // O erro é impresso com uma mensagem explicativa.
+                System.out.println("Erro ao atualizar usuário: " + e.getMessage());
+            }
+        
+        return
+                "tarefa atualizada";
+    
+        
+} //atualizar tarefa
+} //end
+    
+
